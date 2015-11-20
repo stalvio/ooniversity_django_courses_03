89879fd2fb9b7e0f13b66e8c07d814e8e79e634c
Stalvio Neto <stals_007@mail.ru>
2015-11-20 19:35:19 +0200

    add vote funcrion in views.py

--- polls/views.py
+++ polls/views.py
@@ -24,7 +24,20 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def vote(request, question_id):
-	model = Question
-	template_name = 'polls/results.html'
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
