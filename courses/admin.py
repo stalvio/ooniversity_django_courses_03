17f156d4acf0fde78116974da9b2ece3431c3ee2
Stalvio Neto <stals_007@mail.ru>
2015-11-28 05:21:31 +0200

    modifened admin.py files

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
+class LessonInline(admin.TabularInline):
+	model = Lesson
+	list_display = ['subject', 'description', 'order']	
+	extra = 0
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+	list_display = ['name', 'short_description']
+	fields = ['name', 'short_description', 'description']
+	inlines = [LessonInline]
+	search_fields = ['name']	
+
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+	search_fields = ['surname', 'email']
+	list_display = ['full_name', 'email', 'skype']
+	list_filter = ['courses']
+	filter_horizontal = ['courses']
+	def full_name(self, obj):
+	    return ("%s %s" % (obj.name, obj.surname))
+	
+	fieldsets = (
+		('Personal info', { 'fields': ('name', 'surname', 'date_of_birth')}), 
+		('Contact info', { 'fields': ('email', 'phone', 'address', 'skype')}),
+		(None, { 'fields': ('courses', ),}), 
+	             )
+
+admin.site.register(Student, StudentAdmin)
