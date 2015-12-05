cf05c781315a8b74275b92c9e3f0462e93b2aac3
Stalvio Neto <stals_007@mail.ru>
2015-12-05 18:10:16 +0200

    7_3

--- students/urls.py
+++ students/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
 	
 	url(r'^$', views.list_view, name='list_view'),
 	url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
-	url(r'add/$', views.create, name='create'),
+	url(r'add/$', views.create, name='add'),
 	url(r'edit/(?P<pk>\d+)/$', views.edit, name='edit'),
 	url(r'remove/(?P<pk>\d+)/$', views.remove, name='remove'),
 	# url(r'^results/a\=(?P<a>\W{0,30}\w{0,30})\&b\=(?P<b>\W{0,30}\w{0,30})\&c\=(?P<c>\W{0,30}\w{0,30})/$', views.quadratic_results, name='quadratic_results'),
